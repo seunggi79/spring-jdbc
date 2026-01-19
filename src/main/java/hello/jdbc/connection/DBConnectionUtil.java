bf6fff1a3a7fb125d7a252810c09868cbfb14e3f
seunggi <hsg77689469.gamil.com>
2026-01-19 19:24:40 +0900

    JDBC 개발 - 등록

--- src/main/java/hello/jdbc/connection/DBConnectionUtil.java
+++ src/main/java/hello/jdbc/connection/DBConnectionUtil.java
@@ -11,8 +11,7 @@ public class DBConnectionUtil {
         try {
             Connection connection = DriverManager.getConnection(URL, USERNAME,
                     PASSWORD);
-            log.info("get connection={}, class={}", connection,
-                    connection.getClass());
+            log.info("get connection={}, class={}", connection, connection.getClass());
             return connection;
         } catch (SQLException e) {
             throw new IllegalStateException(e);
